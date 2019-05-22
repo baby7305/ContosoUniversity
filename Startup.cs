8f8d923989573d89219c6707b3ab5e1b629201e7
baby7305 <songhaibei7305@163.com>
2019-05-22 13:19:50 +0800

    格式化Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Models;
 
 namespace ContosoUniversity {
     public class Startup {
@@ -29,8 +29,8 @@ namespace ContosoUniversity {
 
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
 
-    services.AddDbContext<SchoolContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
+            services.AddDbContext<SchoolContext> (options =>
+                options.UseSqlServer (Configuration.GetConnectionString ("SchoolContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
