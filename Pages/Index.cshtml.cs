db5208a60483189c66124d4c3151d094b5d3c0c4
baby7305 <songhaibei7305@163.com>
2019-05-22 11:28:09 +0800

    格式化cs文件

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,12 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ContosoUniversity.Pages
-{
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace ContosoUniversity.Pages {
+    public class IndexModel : PageModel {
+        public void OnGet () {
 
         }
     }
