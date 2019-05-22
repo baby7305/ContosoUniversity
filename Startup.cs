3abd5f3b0033ab29e46b7eba4bc48302707c5b9e
baby7305 <songhaibei7305@163.com>
2019-05-22 13:16:04 +0800

    生成Student前后端代码

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
 
 namespace ContosoUniversity {
     public class Startup {
@@ -26,6 +28,9 @@ namespace ContosoUniversity {
             });
 
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+
+    services.AddDbContext<SchoolContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
