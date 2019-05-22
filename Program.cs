fedf6e4ac2574b58ed37f340dde7fd3e83e6d8ef
baby7305 <songhaibei7305@163.com>
2019-05-22 13:20:32 +0800

    更新main方法

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ContosoUniversity.Models; // SchoolContext
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection; // CreateScope
+using System;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoUniversity {
     public class Program {
         public static void Main (string[] args) {
-            CreateWebHostBuilder (args).Build ().Run ();
+            var host = CreateWebHostBuilder (args).Build ();
+
+            using (var scope = host.Services.CreateScope ()) {
+                var services = scope.ServiceProvider;
+
+                try {
+                    var context = services.GetRequiredService<SchoolContext> ();
+                    context.Database.EnsureCreated ();
+                } catch (Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>> ();
+                    logger.LogError (ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run ();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
