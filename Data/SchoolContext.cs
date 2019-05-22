b6d61dcdfa398a13ac5486d6ebfe3fdee16afbe7
baby7305 <songhaibei7305@163.com>
2019-05-22 13:23:35 +0800

    格式化SchoolContext.cs

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace ContosoUniversity.Models
-{
-    public class SchoolContext : DbContext
-    {
-        public SchoolContext (DbContextOptions<SchoolContext> options)
-            : base(options)
-        {
-        }
+namespace ContosoUniversity.Models {
+    public class SchoolContext : DbContext {
+        public SchoolContext (DbContextOptions<SchoolContext> options) : base (options) { }
 
         public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
     }
